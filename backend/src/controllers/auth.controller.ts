b2bcdde57c334f0a1e278e7fa0ee09ab27f9b4bc
Ashishbhandari5 <ashishbhandari19991@gmail.com>
2024-10-03 10:23:08 +0530

    search

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -90,7 +90,7 @@ export const getMe = async (req: Request, res: Response) => {
   try {
     const user = await prisma.user.findUnique({ where: { id: req.user.id } });
     if (!user) {
-      return res.status(500).json({ error: "User not found" });
+      return res.status(404).json({ error: "User not found" });
     }
     res.status(200).json({
       id: user.id,
